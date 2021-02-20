715aecaa128a9f4f182088aa892762b5ae033541
Selman Yusufoğlu <42515502+kmnsys@users.noreply.github.com>
2021-02-20 20:59:35 +0300

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -1,3 +1,4 @@
 import splitfolders
 
+#To split dataset https://github.com/jfilter/split-folders library has been used.
 splitfolders.ratio("D:/CroppedYale", output="D:/data2", seed=1337, ratio=(.7, .0, .3), group_prefix=None) # default values
